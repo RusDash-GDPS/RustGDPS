0c23c5c76ea19e58e724ea044af22a348d157d9a
RusDash <russiandashgdps@gmail.com>
2025-03-22 21:25:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const { Client, IntentsBitField } = require('discord.js');
 
-const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ],
-});
-
-client.on('ready', (c) => {
-    console.log('RusDash Бот Готов Служить Клиентам!');
-})
-
-client.login("MTA3MzUwNTQ2NDY0NzY5NjQzNA.GiHr_P.ifoqSg-m33KlT7Kdxin3ALtz1UyWhVGFCQrwV8");
